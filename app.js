d4a31bff6a274893e62773f7540deeb108134016
Pete Faretra <55142305+peterston-e@users.noreply.github.com>
2024-02-29 11:22:45 +0000

    ticket 4

--- app.js
+++ app.js
@@ -21,6 +21,21 @@ app.get("/quotes", async function (req, res) {
 	res.json(quotes);
 });
 
+app.get("/quotes/:id", async function (req, res) {
+	// get the id from params
+	const id = req.params.id;
+	// used id in function call and save variable
+	const quote = await getQuoteByID(id);
+	// if x is null - respond with error message
+	if (quote === null) {
+		res.status(404).json({ error: "quote not found" });
+	}
+	// else response with quote
+	else {
+		res.json(quote);
+	}
+});
+
 app.listen(PORT, function () {
 	console.log(`Server is now listening on http://localhost:${PORT}`);
 });
