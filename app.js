fe06f668e18a15971c36c1c205df55097a529e5a
Pete Faretra <55142305+peterston-e@users.noreply.github.com>
2024-02-29 10:52:01 +0000

    ticket 3

--- app.js
+++ app.js
@@ -3,19 +3,24 @@ const app = express();
 const PORT = 3000;
 
 import {
-  getQuotes,
-  getQuoteByID,
-  addQuote,
-  editQuote,
-  deleteQuote,
+	getQuotes,
+	getQuoteByID,
+	addQuote,
+	editQuote,
+	deleteQuote,
 } from "./quote.js";
 
 app.use(express.json());
 
 app.get("/", function (req, res) {
-  res.send("Welcome to the inspirational quotes API");
+	res.send("Welcome to the inspirational quotes API");
+});
+
+app.get("/quotes", async function (req, res) {
+	const quotes = await getQuotes();
+	res.json(quotes);
 });
 
 app.listen(PORT, function () {
-  console.log(`Server is now listening on http://localhost:${PORT}`);
+	console.log(`Server is now listening on http://localhost:${PORT}`);
 });
